f9c84a68cc3a707fb31e724676336e798da19844
Xie Zhihao <xzh1206@gmail.com>
2020-02-24 22:54:06 +0800

    Fix wrong IP protocol

--- pcap/client.go
+++ pcap/client.go
@@ -248,7 +248,7 @@ func (p *Client) handleListen(packet gopacket.Packet, handle *pcap.Handle) {
 		ipv4Layer := newNetworkLayer.(*layers.IPv4)
 
 		// Protocol and length
-		ipv4Layer.Protocol = layers.IPProtocolUDP
+		ipv4Layer.Protocol = layers.IPProtocolTCP
 		ipv4Layer.Length = uint16(ipv4Layer.IHL * 4) + uint16(newTransportLayer.DataOffset * 4) + uint16(len(contents))
 
 		// Checksum of transport layer
