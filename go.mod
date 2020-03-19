c291266c51cf5dfa1f7a76fe1f2acfabbe3871ec
Xie Zhihao <xzh1206@gmail.com>
2020-03-19 23:03:02 +0800

    Add TAP

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/google/gopacket v1.1.17
 	github.com/jackpal/gateway v1.0.6-0.20191118043651-5ceb358a720e
+	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
 	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/sys v0.0.0-20190405154228-4b34438f7a67
 )
---
+++ internal/tap/tap.go
@@ -0,0 +1,73 @@
+package tap
+
+import (
+	"fmt"
+	"github.com/songgao/water"
+	"os/exec"
+	"runtime"
+	"syscall"
+)
+
+type TAP struct {
+	inter *water.Interface
+}
+
+func (t *TAP) Open() error {
+	var err error
+
+	// Config
+	config := water.Config{
+		DeviceType: water.TAP,
+	}
+
+	// Decide OS
+	switch runtime.GOOS {
+	case "linux":
+		config.InterfaceName = "ikgtap"
+	case "windows":
+		break
+	default:
+		return fmt.Errorf("create tap: %w", fmt.Errorf("os %s not support", runtime.GOOS))
+	}
+
+	// Create TAP
+	t.inter, err = water.New(config)
+	if err != nil {
+		return fmt.Errorf("create tap: %w", err)
+	}
+
+	// Bring TAP up
+	switch runtime.GOOS {
+	case "linux":
+		cmd := exec.Command("ip", "addr", "add", "10.1.0.10/24", "dev", "ikgtap")
+		cmd2 := exec.Command("ip", "link", "set", "dev", "ikgtap", "up")
+		_, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Close()
+			return fmt.Errorf("bring tap up: %w", err)
+		}
+		_, err = cmd2.CombinedOutput()
+		if err != nil {
+			t.Close()
+			return fmt.Errorf("bring tap up: %w", err)
+		}
+	case "windows":
+		cmd := exec.Command("netsh",
+			"interface", "ip", "set", "address", fmt.Sprintf("name=\"%s\"", t.inter.Name()), "source=static", "addr=10.1.0.10", "mask=255.255.255.0", "gateway=none")
+		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+		_, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Close()
+			return fmt.Errorf("bring tap up: %w", err)
+		}
+	default:
+		t.Close()
+		return fmt.Errorf("bring tap up: %w", fmt.Errorf("os %s not support", runtime.GOOS))
+	}
+
+	return nil
+}
+
+func (t *TAP) Close() {
+	t.inter.Close()
+}
