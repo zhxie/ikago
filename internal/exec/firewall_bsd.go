c2b0bdc17909090776efc7a8232a0de1f501bb2c
Xie Zhihao <xzh1206@gmail.com>
2020-04-14 22:04:02 +0800

    Fix firewall rule on BSD

--- internal/exec/firewall_bsd.go
+++ internal/exec/firewall_bsd.go
@@ -36,10 +36,7 @@ func addSpecificFirewallRule(ip net.IP, port uint16) error {
 	}
 
 	routeCmd = exec.Command("pfctl", "-d")
-	_, err = routeCmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("exec: %w", err)
-	}
+	_, _ = routeCmd.CombinedOutput()
 
 	routeCmd = exec.Command("pfctl", "-e")
 	_, err = routeCmd.CombinedOutput()
