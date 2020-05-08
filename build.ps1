5277a681a6cd4c6a31eac6e84c01363fd74e0376
Xie Zhihao <xzh1206@gmail.com>
2020-05-09 00:52:47 +0800

    Add icon and manifest

--- build.ps1
+++ build.ps1
@@ -3,5 +3,6 @@ $GIT_BRANCH=git rev-parse --abbrev-ref HEAD
 $GIT_COMMIT_COUNT=git rev-list --count ${GIT_BRANCH}
 $GIT_COMMIT=git log --pretty=format:"%h" -1
 
+goversioninfo -o .\cmd\ikago-client\main.syso .\build\versioninfo.json
 go build -ldflags="-X main.version=${GIT_TAG} -X main.build=${GIT_COMMIT_COUNT} -X main.commit=${GIT_COMMIT}" ./cmd/ikago-client
 go build -ldflags="-X main.version=${GIT_TAG} -X main.build=${GIT_COMMIT_COUNT} -X main.commit=${GIT_COMMIT}" ./cmd/ikago-server
