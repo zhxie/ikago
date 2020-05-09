d2d4288e1a407bb9633da65851300ad9383494c2
Xie Zhihao <xzh1206@gmail.com>
2020-05-09 16:22:38 +0800

    Structure build

--- build.ps1
+++ build.ps1
@@ -3,6 +3,13 @@ $GIT_BRANCH=git rev-parse --abbrev-ref HEAD
 $GIT_COMMIT_COUNT=git rev-list --count ${GIT_BRANCH}
 $GIT_COMMIT=git log --pretty=format:"%h" -1
 
-goversioninfo -o .\cmd\ikago-client\main.syso .\build\versioninfo.json
-go build -ldflags="-X main.version=${GIT_TAG} -X main.build=${GIT_COMMIT_COUNT} -X main.commit=${GIT_COMMIT}" ./cmd/ikago-client
-go build -ldflags="-X main.version=${GIT_TAG} -X main.build=${GIT_COMMIT_COUNT} -X main.commit=${GIT_COMMIT}" ./cmd/ikago-server
+$VERS=$GIT_TAG.replace("v", "").Split(".")
+$VER_MAIN=$VERS[0] -as [int]
+$VER_SUB=$VERS[1] -as [int]
+$VER_PATCH=$VERS[2] -as [int]
+
+$PROD_VER="$($GIT_TAG)-$($GIT_COMMIT_COUNT) ($($GIT_COMMIT))"
+
+goversioninfo -product-version ${PROD_VER} -ver-major ${VER_MAIN} -ver-minor ${VER_SUB} -ver-patch ${VER_PATCH} -ver-build ${GIT_COMMIT_COUNT} -o .\cmd\ikago-client\main.syso .\build\windows\IkaGo-client.json
+go build -ldflags="-X main.version=${GIT_TAG} -X main.build=${GIT_COMMIT_COUNT} -X main.commit=${GIT_COMMIT}" .\cmd\ikago-client
+go build -ldflags="-X main.version=${GIT_TAG} -X main.build=${GIT_COMMIT_COUNT} -X main.commit=${GIT_COMMIT}" .\cmd\ikago-server
